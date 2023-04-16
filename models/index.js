90a6f8b078eeb7e972fea91ccb932200b6d29a8a
Grace Roh <grace@BIGMINE.attlocal.net>
2023-04-15 23:50:08 -0700

    write code for models/index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: category_id,
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: category_id,
+})
 
 // Products belongToMany Tags (through ProductTag)
+Products.belongsToMany(Tag, {
+  through: {
+    model: ProductTag
+    // unique:
+  },
+  //as:
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag
+    // unique:
+  },
+  //as:
+})
 
 module.exports = {
   Product,
